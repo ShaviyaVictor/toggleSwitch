601c9cba78724d142ad2eb6a3445c7aa23a26746
shaviyavictor <shaviyavictor@gmail.com>
2022-05-18 16:33:27 +0300

    Complete the script logic

---
+++ logic.js
@@ -0,0 +1,21 @@
+const stylesheet = document.documentElement.style;
+const toggle = document.querySelector('.switch_input[data-theme-toggle]');
+
+toggle.addEventListener('click', () => {
+
+  // getting the css variable
+  const color1 = 
+  console.log("🚀 ~ file: logic.js ~ line 8 ~ toggle.addEventListener ~ color1", color1)
+  getComputedStyle(document.documentElement).getPropertyValue(
+    '--color1'
+  );
+  const color2 =
+  console.log("🚀 ~ file: logic.js ~ line 13 ~ toggle.addEventListener ~ color2", color2)
+  getComputedStyle(document.documentElement).getPropertyValue(
+    '--color2'
+  );
+
+  // switching the css variables
+  stylesheet.setProperty('--color1', color2);
+  stylesheet.setProperty('--color2', color1);
+});
